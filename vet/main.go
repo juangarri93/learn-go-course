00e58ad893d5b0ea66d82d559929d93eff0b2ab3
Juan Martin Garrido <juangarri93@Juans-MacBook-Pro.local>
2025-01-26 17:49:02 +0100

    Add barking modes

--- vet/main.go
+++ vet/main.go
@@ -3,14 +3,67 @@ package main
 import (
 	"strconv"
 	"github.com/juangarri93/learn-go-course-dependency"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 const vetName = "Puppies Vet"
 var puppiesAmount = 3
+const parralelBarkingEnabled = true
+const rangeBarksEnabled = true
+
+func init(){
+	println("The vet is opening")
+}
 
 func main(){
 	puppiesAmount++
-	println("In: " + vetName + " the " + strconv.Itoa(puppiesAmount) + " puppies will bark:" + puppy.Bark())
-	println("Or even super bark:" + puppy.SuperBark())
+	fmt.Println("In: " + vetName + " the " + strconv.Itoa(puppiesAmount) + " puppies will bark: " + puppy.Bark())
+	fmt.Println("They can also super bark: " + puppy.SuperBark())
+
+	if parralelBarkingEnabled {
+		parralelBarking()
+	}
+
+	if rangeBarksEnabled {
+		barkRange()
+	}
 }
 
+func parralelBarking() {
+	// Concurrency -> select a channel
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// Random duration
+	d1 := time.Duration(rand.Int63n(250))
+	d2 := time.Duration(rand.Int63n(250))
+
+	go func() {
+		time.Sleep(d1 * time.Millisecond)
+		ch1 <- rand.Int()
+	}()
+
+	go func() {
+		time.Sleep(d2 * time.Millisecond)
+		ch2 <- rand.Int()
+	}()
+
+	// A "select" statement chooses which of a set of possible send or receive operations will proceed.
+	select {
+		case v1 := <- ch1:
+			fmt.Println("The bark came from channel 1: ", v1)
+		case v2 := <- ch2:
+			fmt.Println("The bark came from channel 2: ", v2)
+	}
+}
+
+func barkRange() {
+	// for range loop
+	// data structures - slice
+	xi := []string{"guf", "wof", "waf"}
+	for i, v := range xi {
+		fmt.Println("Ranging over barks", i, v)
+	}
+}
